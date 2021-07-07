0c8e6ceff38c8f8e77097107dbcc432db80d8faf
William <wgenard94@gmail.com>
2021-07-07 16:30:14 +0200

    initial commit

---
+++ src/component/increment/increment.jsx
@@ -0,0 +1,40 @@
+import PropTypes from 'prop-types'
+import { useState } from 'react'
+
+const Increment = function(props) {
+
+    const {inc} = props;
+
+    const [nb, setNb] = useState(0)
+
+    const resetButtonVisible = !(nb === 0)
+
+    const incrementValue = () => {
+        setNb(previousValue => previousValue + inc)
+    }
+
+    const resetValue = () => {
+        setNb(0)
+    }
+
+    return(
+        <>
+            <h3>{nb}</h3>
+            <button onClick={incrementValue}>Add</button>
+            {(resetButtonVisible) 
+                ? <button onClick={resetValue}>Reset</button>
+                : <span></span>
+            }
+        </>
+    )
+}
+
+Increment.propTypes = {
+    inc: PropTypes.number
+}
+
+Increment.defaultProps = {
+    inc : 1
+}
+
+export default Increment;
